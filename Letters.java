fec960998914d6977230a4acfa310d777a4a336c
Banele <Banele@overdrive.co.za>
2019-10-22 15:52:33 +0200

    Initial commit

---
+++ Letters.java
@@ -0,0 +1,9 @@
+public class Letters{
+	public static void main(String[]args){
+		char[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+		for(int a = 0; a < letters.length; a++){
+			while
+			System.out.println(letters[a]);
+		}
+	}
+}
